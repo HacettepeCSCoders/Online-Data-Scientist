77dfba06bad07713a33d5588528c21578120ba25
ugurcanerdogan <ugurcanerdogan3306@gmail.com>
2022-12-11 00:03:11 +0300

    service added

---
+++ java/src/main/java/edu/ods/java/service/AuthService.java
@@ -0,0 +1,84 @@
+package edu.ods.java.service;
+
+import edu.ods.java.config.EmailValidator;
+import edu.ods.java.dto.UserDTO;
+import edu.ods.java.exceptions.UserNotFoundException;
+import edu.ods.java.models.User;
+import edu.ods.java.repository.RoleRepository;
+import edu.ods.java.repository.UserRepository;
+import edu.ods.java.security.JWTGenerator;
+import edu.ods.java.security.JWTRequestModel;
+import edu.ods.java.security.JWTResponseModel;
+import edu.ods.java.security.UserDetailsAdapter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+	private final UserRepository userRepository;
+
+	private final RoleRepository roleRepository;
+
+	private final PasswordEncoder passwordEncoder;
+
+	private final EmailValidator emailValidator;
+
+	private final AuthenticationManager authenticationManager;
+
+	private final JWTGenerator jwtGenerator;
+
+	public User registerHelper(String username, String password) {
+
+		String encodedPsw = passwordEncoder.encode(password);
+		User user = new User();
+		user.setUsername(username);
+		user.setPassword(encodedPsw);
+		var userRoleOpt = roleRepository.findByName("ROLE_USER");
+		userRoleOpt.ifPresent((userRole) -> user.setRoles(Set.of(userRoleOpt.get())));
+		return userRepository.save(user);
+	}
+
+	@Transactional
+	public User register(UserDTO request) {
+
+		boolean isValidEmail = emailValidator.test(request.getUsername());
+		if (!isValidEmail) {
+			throw new IllegalArgumentException("Not a valid email");
+		}
+		if (!userRepository.existsByUsername(request.getUsername())) {
+			return registerHelper(request.getUsername(), request.getPassword());
+		}
+		throw new IllegalArgumentException("Email already taken");
+	}
+
+	public JWTResponseModel createToken(JWTRequestModel request) throws Exception {
+		try {
+			authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+		}
+		catch (DisabledException e) {
+			throw new Exception("USER_DISABLED", e);
+		}
+		catch (BadCredentialsException e) {
+			throw new Exception("INVALID_CREDENTIALS", e);
+		}
+		final User user = userRepository.findByUsername(request.getUsername())
+				.orElseThrow(() -> new UserNotFoundException("User could not be found"));
+		final UserDetailsAdapter userDetails = new UserDetailsAdapter(user);
+		final String jwtToken = jwtGenerator.generateJwtToken(userDetails);
+
+		return new JWTResponseModel(jwtToken, userDetails.user().getId(), !userDetails.isEnabled(),
+				userDetails.user().getUsername());
+	}
+
+}
---
+++ java/src/main/java/edu/ods/java/service/UserService.java
@@ -0,0 +1,131 @@
+package edu.ods.java.service;
+
+import edu.ods.java.dto.UserDTO;
+import edu.ods.java.dto.UserUpdateDTO;
+import edu.ods.java.exceptions.UserNotFoundException;
+import edu.ods.java.models.Role;
+import edu.ods.java.models.User;
+import edu.ods.java.repository.RoleRepository;
+import edu.ods.java.repository.UserRepository;
+import edu.ods.java.security.UserDetailsAdapter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+	private final UserRepository userRepository;
+
+	private final RoleRepository roleRepository;
+
+	private final PasswordEncoder passwordEncoder;
+
+	public UserDTO createUser(UserDTO userDTO) {
+
+		User user = new User();
+		user.setUsername(userDTO.getUsername());
+		user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+		Optional<Role> userRoleOpt = roleRepository.findByName("ROLE_USER");
+		userRoleOpt.ifPresent((userRole) -> user.setRoles(Set.of(userRoleOpt.get())));
+		return mapToDTO(userRepository.save(user));
+	}
+
+	public List<UserDTO> getUsersWithRole(List<String> roles) {
+		return userRepository.findByRoles_NameIn(roles).stream().map(this::mapToDTO).collect(Collectors.toList());
+	}
+
+	public List<UserDTO> getAllUsers() {
+		return userRepository.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
+	}
+
+	public UserDTO getByUsername(String username) {
+		return mapToDTO(userRepository.findByUsername(username)
+				.orElseThrow(() -> new UserNotFoundException("User could not be found")));
+	}
+
+	public List<UserDTO> getAllByUsername(String username) {
+		return userRepository.findByUsernameContaining(username).stream().map(this::mapToDTO)
+				.collect(Collectors.toList());
+	}
+
+	public UserDTO getById(long id) {
+		return mapToDTO(
+				userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User could not be found")));
+	}
+
+	public Page<UserDTO> getAllWithJpaPagination(int pageNumber, int pageSize, String username) {
+		Page<User> paged = userRepository.findAllByActiveTrueAndUsernameNot(username,
+				PageRequest.of(pageNumber, pageSize));
+		return paged.map(this::mapToDTO);
+	}
+
+	public UserDTO updateUser(long id, UserUpdateDTO userUpdateDTO) {
+
+		User user = userRepository.findById(id)
+				.orElseThrow(() -> new UserNotFoundException("User could not be updated"));
+
+		if (userUpdateDTO.getUsername() != null) {
+			user.setUsername(userUpdateDTO.getUsername());
+		}
+		if (userUpdateDTO.getPassword() != null) {
+			user.setPassword(passwordEncoder.encode(userUpdateDTO.getPassword()));
+		}
+		User updatedUser = userRepository.save(user);
+		return mapToDTO(updatedUser);
+	}
+
+	public UserDTO removeUser(long id) {
+		User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User could not be found"));
+		user.setActive(false);
+		return mapToDTO(userRepository.save(user));
+	}
+
+	public UserDTO setUserActive(long id) {
+		User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User could not be found"));
+		user.setActive(true);
+		return mapToDTO(userRepository.save(user));
+	}
+
+	public void deleteUser(long id) {
+		User user = userRepository.findById(id)
+				.orElseThrow(() -> new UserNotFoundException("User could not be delete"));
+		userRepository.delete(user);
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UserNotFoundException {
+		User user = userRepository.findByUsername(username)
+				.orElseThrow(() -> new UserNotFoundException("User could not be found"));
+		return new UserDetailsAdapter(user);
+	}
+
+	public boolean userExists(String email) {
+		return userRepository.findByUsername(email).isPresent();
+	}
+
+	private UserDTO mapToDTO(User user) {
+		UserDTO userDTO = new UserDTO();
+		userDTO.setUsername(user.getUsername());
+		userDTO.setPassword(user.getPassword());
+		return userDTO;
+	}
+
+	private User mapToEntity(UserDTO userDTO) {
+		User user = new User();
+		user.setUsername(userDTO.getUsername());
+		user.setPassword(userDTO.getPassword());
+		return user;
+	}
+
+}
