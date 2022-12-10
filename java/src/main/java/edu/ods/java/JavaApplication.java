2b552d73eb0e25c8b06325592ccd7d1e3f2e6c25
ugurcanerdogan <ugurcanerdogan3306@gmail.com>
2022-12-11 00:06:12 +0300

    main class

--- java/src/main/java/edu/ods/java/JavaApplication.java
+++ java/src/main/java/edu/ods/java/JavaApplication.java
@@ -2,8 +2,10 @@ package edu.ods.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin
 public class JavaApplication {
 
 	public static void main(String[] args) {
