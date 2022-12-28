a76952d4780cc82751ca8e69bf47266c5049227e
brkdglr <dagburak3@gmail.com>
2022-12-28 23:39:08 +0300

    getCSVDatas

--- reactJS/frontend/src/hocs/dataContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-const DataContext = createContext({
-  rowData: [],
-  orFilteredRowData: [],
-  setOrFilteredRowData: () => {},
-});
-
-export default DataContext;
--- reactJS/frontend/src/views/workspace/index.js
+++ reactJS/frontend/src/views/workspace/index.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { Layout, Steps, Button, message } from "antd";
 import { steps } from "../../utils/workspace/steps";
-const { Content, Header, Sider } = Layout;
+import { DataProvider } from "../../hocs/dataProvider";
+const { Content, Sider } = Layout;
 
 const Workspace = () => {
   const [current, setCurrent] = useState(0);
@@ -18,36 +19,42 @@ const Workspace = () => {
 
   return (
     <>
-      <Layout className="layout-background">
-        <Layout>
-          <Content className="content-nav">
-            <div className="steps-content">{steps[current].content}</div>
-            <div className="steps-action">
-              {current < steps.length - 1 && (
-                <Button type="primary" onClick={() => next()}>
-                  Next
-                </Button>
-              )}
-              {current === steps.length - 1 && (
-                <Button
-                  type="primary"
-                  onClick={() => message.success("Processing complete!")}
-                >
-                  Done
-                </Button>
-              )}
-              {current > 0 && (
-                <Button style={{ margin: "0 8px" }} onClick={() => prev()}>
-                  Previous
-                </Button>
-              )}
-            </div>
-          </Content>
-          <Sider className="workspace-header">
-            <Steps direction="vertical" current={current} items={items}></Steps>
-          </Sider>
+      <DataProvider>
+        <Layout className="layout-background">
+          <Layout>
+            <Content className="content-nav">
+              <div className="steps-content">{steps[current].content}</div>
+              <div className="steps-action">
+                {current < steps.length - 1 && (
+                  <Button type="primary" onClick={() => next()}>
+                    Next
+                  </Button>
+                )}
+                {current === steps.length - 1 && (
+                  <Button
+                    type="primary"
+                    onClick={() => message.success("Processing complete!")}
+                  >
+                    Done
+                  </Button>
+                )}
+                {current > 0 && (
+                  <Button style={{ margin: "0 8px" }} onClick={() => prev()}>
+                    Previous
+                  </Button>
+                )}
+              </div>
+            </Content>
+            <Sider className="workspace-header">
+              <Steps
+                direction="vertical"
+                current={current}
+                items={items}
+              ></Steps>
+            </Sider>
+          </Layout>
         </Layout>
-      </Layout>
+      </DataProvider>
     </>
   );
 };
