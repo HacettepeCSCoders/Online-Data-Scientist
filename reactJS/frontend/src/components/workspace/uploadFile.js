90e0fdbe75b3d3a15fdd84fbb78d18e326675669
brkdglr <dagburak3@gmail.com>
2023-01-07 02:40:26 +0300

    excelVisualization handled

--- reactJS/frontend/src/components/workspace/uploadFile.js
+++ reactJS/frontend/src/components/workspace/uploadFile.js
@@ -1,17 +1,50 @@
 import React, { useEffect, useState } from "react";
 import { Upload } from "antd";
 import { useData } from "../../hocs/dataProvider";
+import * as XLSX from "xlsx";
 const { Dragger } = Upload;
 
 const UploadFile = () => {
   const { dataDetails, setDataDetails } = useData();
   const [data, setData] = useState();
 
+  const readExcel = (file) => {
+    const promise = new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
+
+      fileReader.onload = (e) => {
+        const bufferArray = e.target.result;
+        const wb = XLSX.read(bufferArray, { type: "buffer" });
+        const wsname = wb.SheetNames[0];
+        const ws = wb.Sheets[wsname];
+        const data = XLSX.utils.sheet_to_csv(ws);
+
+        resolve(data);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+
+    promise.then((d) => {
+      console.log(d);
+      setData(d);
+    });
+  };
+
+  const readCSV = (file) => {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      console.log(e.target.result);
+      setData(e.target.result);
+    };
+    reader.readAsText(file);
+  };
+
   useEffect(() => {
-    console.log("Data");
-    console.log(data);
     setDataDetails(data);
-    console.log(dataDetails);
   });
 
   // hasHeader and csvDelimeter check buttons should be added ???????????????
@@ -19,19 +52,19 @@ const UploadFile = () => {
   return (
     <>
       <Dragger
-        accept=".txt, .csv"
+        accept=".txt, .csv, .xlsx, .xls"
         showUploadList={true}
         multiple={false}
         beforeUpload={(file) => {
-          const reader = new FileReader();
-
-          reader.onload = (e) => {
-            console.log(e.target.result);
-            setData(e.target.result);
-          };
-          reader.readAsText(file);
-
-          // Prevent upload
+          const fileName = file.name;
+          const fileNameArray = fileName.split(".");
+          const fileExtension = fileNameArray[1];
+          console.log(fileExtension);
+          if (fileExtension === "txt" || fileExtension === "csv") {
+            readCSV(file);
+          } else if (fileExtension === "xlsx" || fileExtension === "xls") {
+            readExcel(file);
+          }
           return false;
         }}
       >
