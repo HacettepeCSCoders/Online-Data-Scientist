19fc07b500e4972ce8fba12d39ca9670a5d816cf
brkdglr <dagburak3@gmail.com>
2023-02-28 22:17:51 +0300

    topBar updated

--- reactJS/frontend/src/components/common/topBar/index.js
+++ reactJS/frontend/src/components/common/topBar/index.js
@@ -1,17 +1,60 @@
 import React from "react";
-import { Layout, Menu } from "antd";
+import { Layout, Avatar, Row, Col, Dropdown, Input } from "antd";
+import {
+  UserOutlined,
+  SearchOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
+
 const { Header } = Layout;
 
 const TopBar = () => {
+  const onClickLogout = () => {};
+
+  const items = [
+    {
+      key: "1",
+      label: <div> Signed as burakdag</div>,
+      disabled: true,
+    },
+    {
+      key: "2",
+      label: <a href="/settings">Settings</a>,
+    },
+    {
+      key: "3",
+      label: <a href="/burakdag">Your Profile</a>,
+    },
+    {
+      key: "4",
+      label: (
+        <div onClick={onClickLogout}>
+          Sign Out <LogoutOutlined />
+        </div>
+      ),
+    },
+  ];
   return (
     <>
       <Header className="header-nav">
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={["2"]}
-          className="header-menu"
-        />
+        <Row gutter={16}>
+          <Col span={8} offset={4}>
+            <Input
+              prefix={<SearchOutlined className="site-form-item-icon" />}
+              placeholder="Search..."
+              type="text"
+            />
+          </Col>
+          <Col className="gutter-row" span={2} offset={6}>
+            <Dropdown
+              menu={{
+                items,
+              }}
+            >
+              <Avatar size={48} icon={<UserOutlined />} />
+            </Dropdown>
+          </Col>
+        </Row>
       </Header>
     </>
   );
