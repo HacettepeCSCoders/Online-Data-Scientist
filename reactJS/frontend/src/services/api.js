26c1eddf8b247758269c04ccaad040306eb49b06
ugurcanerdogan <ugurcanerdogan3306@gmail.com>
2023-03-07 13:09:24 +0300

    file structures changed

--- reactJS/frontend/src/services/api.js
+++
@@ -1,3 +0,0 @@
-import store from "store";
-
-export const setAuth = (auth) => store.set("_aut", auth);
--- reactJS/frontend/src/views/mainPage/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MainPage = () => {
-  return <>MainPage</>;
-};
-
-export default MainPage;
