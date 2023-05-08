36b382435159d561dff0cdbebcd6395980745e27
brkdglr <dagburak3@gmail.com>
2023-05-09 00:55:25 +0300

    backend db connection corrected

--- react/src/services/processService.js
+++ react/src/services/processService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const startProcess = async (dataAndProcess) => {
   return axios({
     method: "post",
-    url: `http://localhost:8080/api/v1/auth/login`,
+    url: `http://localhost:8000/insert`,
     data: dataAndProcess,
     headers: {
       "Content-Type": "application/json",
