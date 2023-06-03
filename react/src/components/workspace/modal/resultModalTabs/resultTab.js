f7f9087a838a379de2d1ee526749f5b18b716433
brkdglr <dagburak3@gmail.com>
2023-06-03 16:05:27 +0300

    export pdf created

--- react/src/components/workspace/modal/resultModalTabs/resultTab.js
+++ react/src/components/workspace/modal/resultModalTabs/resultTab.js
@@ -1,7 +1,42 @@
 import React from "react";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
+import { Descriptions } from "antd";
 
 const ResultTab = () => {
-  return <>Result</>;
+  const printRef = React.useRef();
+
+  const generatePDF = async () => {
+    const element = printRef.current;
+    const canvas = await html2canvas(element);
+    const data = canvas.toDataURL("image/png");
+
+    const pdf = new jsPDF();
+    const imgProperties = pdf.getImageProperties(data);
+    const pdfWidth = pdf.internal.pageSize.getWidth();
+    const pdfHeight = (imgProperties.height * pdfWidth) / imgProperties.width;
+
+    pdf.addImage(data, "PNG", 0, 0, pdfWidth, pdfHeight);
+    pdf.save("report.pdf");
+  };
+  return (
+    <>
+      <div ref={printRef}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </div>
+      <button onClick={generatePDF} type="button">
+        Export PDF
+      </button>
+    </>
+  );
 };
 
 export default ResultTab;
