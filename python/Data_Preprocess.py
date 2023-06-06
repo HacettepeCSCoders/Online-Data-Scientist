36327f1d3d2e42d7ad20aa2c5b769e345c19ba1d
ugurcanerdogan <ugurcanerdogan3306@gmail.com>
2023-06-06 23:46:29 +0300

    update python

--- python/Data_Preprocess.py
+++ python/Data_Preprocess.py
@@ -622,11 +622,11 @@ def __get_pg_datatypes__(df):
 # function for connecting to database
 def __connect_to_db__(user, password, host, port, name):
     db_uri = f'postgresql://{user}:{password}@{host}:{port}/{name}'
-    try:
-        engine = db.create_engine(db_uri)
-        con = engine.connect()
-    except Exception:
-        raise HTTPException(status_code=400, detail="Can't connect to database")
+    # try:
+    engine = db.create_engine(db_uri)
+    con = engine.connect()
+    # except Exception:
+    #     raise HTTPException(status_code=400, detail="Can't connect to database")
     return con
 
 
