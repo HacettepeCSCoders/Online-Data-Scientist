003a39f94ccd0ba0b35e5a582bfc2f12389dfd6d
ugurcanerdogan <ugurcanerdogan3306@gmail.com>
2023-06-08 23:29:45 +0300

    Revert "Revert "updated master""
    This reverts commit e3fa214012c38634801252d585fa0f4075667dae.

--- react/src/services/workspaceService.js
+++ react/src/services/workspaceService.js
@@ -3,14 +3,14 @@ import axios from "axios";
 export const getAllWorkspaces = (userId) => {
   return axios({
     method: "get",
-    url: `/api/v1/users/${userId}/workspaces`,
+    url: `http://localhost:8080/api/v1/users/${userId}/workspaces`,
   });
 };
 
 export const createWorkspace = (creds, userId) => {
   return axios({
     method: "post",
-    url: `/api/v1/users/${userId}/workspaces`,
+    url: `http://localhost:8080/api/v1/users/${userId}/workspaces`,
     data: creds,
   });
 };
@@ -18,13 +18,13 @@ export const createWorkspace = (creds, userId) => {
 export const removeWorkspace = (workspaceId) => {
   return axios({
     method: "post",
-    url: `/api/v1/workspaces/deactivate/${workspaceId}`,
+    url: `http://localhost:8080/api/v1/workspaces/deactivate/${workspaceId}`,
   });
 };
 
 export const getWorkspace = (userId, workspaceId) => {
   return axios({
     method: "get",
-    url: `/api/v1/users/${3}`,
+    url: `http://localhost:8080/api/v1/users/${3}`,
   });
 };
