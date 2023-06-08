240d1d1f2235dc3c9fd9e8540d6a3da70a441aaf
brkdglr <dagburak3@gmail.com>
2023-06-08 19:20:30 +0300

    all Statistical Test Added

--- react/src/utils/workspace/infoModalStatisticalTest.js
+++ react/src/utils/workspace/infoModalStatisticalTest.js
@@ -1,22 +1,98 @@
 export const infoModalStatisticalTest = [
   {
-    title: "infoModalShapiroWilk",
+    title: "Shapiro Wilk",
     element:
       "The Shapiro Wilk test can be used to decide whether or not a sample fits a normal distribution, and it is commonly used for small samples.",
+    name: "shapiro_wilk",
   },
   {
-    title: "infoModalDagostinoK2",
+    title: "DagostinoK2",
     element:
       "D'Agostino-Pearson's K² test calculates two statistical parameters, named kurtosis and skewness, to determine if the data distribution departs from the normal distribution.",
+    name: "dagostino_k2",
   },
   {
-    title: "infoModalAndersonDarling",
+    title: "Anderson Darling",
     element:
       "The Anderson-Darling statistic measures how well the data follow a particular distribution. For a specified data set and distribution, the better the distribution fits the data, the smaller this statistic will be.",
+    name: "anderson_darling",
   },
   {
-    title: "infoModalPearson",
+    title: "Pearson Correlecation",
     element:
-      "The Anderson-Darling statistic measures how well the data follow a particular distribution. For a specified data set and distribution, the better the distribution fits the data, the smaller this statistic will be.",
+      "The Pearson correlation measures the strength of the linear relationship between two variables. It has a value between -1 to 1, with a value of -1 meaning a total negative linear correlation, 0 being no correlation, and + 1 meaning a total positive correlation..",
+    name: "pearson_correlation",
+  },
+  {
+    title: "Spearman Correlecation",
+    element:
+      "Spearman rank correlation: Spearman rank correlation is a non-parametric test that is used to measure the degree of association between two variables.",
+    name: "spearman_correlation",
+  },
+  {
+    title: "Kendall Correlecation",
+    element:
+      "Kendall rank correlation is used to test the similarities in the ordering of data when it is ranked by quantities.",
+    name: "kendall_correlation",
+  },
+  {
+    title: "Chi Square",
+    element:
+      "Chi-square is a statistical test that examines the differences between categorical variables from a random sample in order to determine whether the expected and observed results are well-fitting.",
+    name: "chi_square",
+  },
+  {
+    title: "Augmented Dickey-Fuller",
+    element:
+      "Augmented Dickey-Fuller  tests the null hypothesis that a unit root is present in a time series sample. The alternative hypothesis is different depending on which version of the test is used, but is usually stationarity or trend-stationarity.",
+    name: "augmented_dickey_fuller",
+  },
+  {
+    title: "Kwiatkowski",
+    element:
+      "The KPSS test is used to determine the absence of a unit root in the time series. If the null hypothesis is not rejected, it indicates that the time series is stationary.",
+    name: "kwiatkowski",
+  },
+  {
+    title: "Student T",
+    element:
+      "The t test estimates the true difference between two group means using the ratio of the difference in group means over the pooled standard error of both groups.",
+    name: "student_t",
+  },
+  {
+    title: "Paired Student T",
+    element:
+      "The paired t test provides an hypothesis test of the difference between population means for a pair of random samples whose differences are approximately normally distributed.",
+    name: "paired_student_t",
+  },
+  {
+    title: "Analysis Of Variance",
+    element:
+      "Analysis of Variance (ANOVA) is a statistical formula used to compare variances across the means (or average) of different groups. A range of scenarios use it to determine if there is any difference between the means of different groups.",
+    name: "analysis_of_variance",
+  },
+  {
+    title: "Mann Whitney U ",
+    element:
+      "The Mann Whitney U test, sometimes called the Mann Whitney Wilcoxon Test or the Wilcoxon Rank Sum Test, is used to test whether two samples are likely to derive from the same population (i.e., that the two populations have the same shape).",
+    name: "mann_whitney_u",
+  },
+  {
+    title: "Wilcoxon Rank Sum ",
+    element:
+      "Wilcoxon rank-sum test is used to compare two independent samples, while Wilcoxon signed-rank test is used to compare two related samples, matched samples, or to conduct a paired difference test of repeated measurements on a single sample to assess whether their population mean ranks differ.",
+    name: "wilcoxon_signed_rank",
+  },
+  {
+    title: "Kruskal-Wallis H",
+    element:
+      "Under Classical Statistics, the Kruskal-Wallis H test is used to test the null hypothesis that all k independent samples come from populations having equal means against the alternative hypothesis that at least one population varies.",
+    name: "kruskal_wallis",
+  },
+  {
+    title: "Friedman ",
+    element:
+      "The Friedman test is used for one-way repeated measures analysis of variance by ranks. In its use of ranks it is similar to the Kruskal–Wallis one-way analysis of variance by ranks. The Friedman test is widely supported by many statistical software packages.",
+    name: "friedman",
   },
 ];
